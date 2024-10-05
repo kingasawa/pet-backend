ee12ee15c73674be944d7c61e2daf425d2d77d45
Khanh Tran <khanhtc@icd-vn.com>
2024-10-05 21:42:31 +0700

    deployment

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: '*', // Hoặc liệt kê các domain cụ thể
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
   };
   app.enableCors(corsOptions);
@@ -32,7 +32,7 @@ async function bootstrap() {
     console.log('id', id);
     done(null, id);
   });
-  console.log(`NestJS v2 đang chạy trên port 3001`);
-  await app.listen(3001);
+  console.log(`NestJS v2 đang chạy trên port 8080`);
+  await app.listen(8080);
 }
 bootstrap();
